bd524bf8ca8f7abef69ade0ae962d420d130c668
swayamgovil <128519418+swayamgovil@users.noreply.github.com>
2024-12-11 21:13:00 +0530

    Update ThisCode.cpp

--- ThisCode.cpp
+++ ThisCode.cpp
@@ -4,10 +4,11 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <set>
 
 using namespace std;
 
-void read_file(vector<vector<string>> &data, string line){   
+void read_file(vector<vector<string>> &data, string line, vector<string>& users, vector<string>& movies){   
     // Open the CSV file
 
     ifstream file("movie_ratings.csv");
@@ -31,25 +32,72 @@ void read_file(vector<vector<string>> &data, string line){
         data.push_back(row);
     }
 
-    file.close();
+    file.close();    
+    set<string> user; 
+    set<string> movie;
+
+    for(int i=1;i<data.size();i++){
+        user.insert(data[i][0]);
+        movie.insert(data[i][1]);
+    }    
+
+    for (const auto& elem : user) {
+        users.push_back(elem);
+    } 
+    for (const auto& elem : movie) {
+        movies.push_back(elem);
+    } 
+}
+
+
+void similarities(vector<vector<string>> data, map<string, float>&avg_rating){    
+
+    float count=0,sum=0;    
+    int change=1;
+
+    for(int i=1;i<data.size();i++){   
+        sum+=stof(data[i][2]);  
+
+        if(stof(data[i][2])>0){
+            count++;
+        }   
+        change++;
+
+        if(change>3){  
+            avg_rating[data[i][0]]=(sum/count);
+            sum=0;
+            count=0;
+        }
+    }    
+
+    for(int i=0;i<3;i++){
+        for(int j=i+1;j<3;j++){
+
+            if(avg_rating[])
+        }
+    }
+    
 
 }
 
-void recommend(vector<vector<string>> data, map <string, float>&ans){   
+void recommend(vector<vector<string>> data, map <string, float>&ratings, vector<string> movies){   
     //Calculate average recommendation of every movie
 
-    for(int i=1;i<=3;i++){    
+    for(int i=0;i<movies.size();i++){    
         float sum=0,count=0;
 
-        for(int j=i;j<data.size();j+=3){
+        string temp=movies[i];
 
-            sum+=std::stof(data[j][2]);   
-            if(std::stof(data[j][2])>0){
-                count++;
-            }   
-        }    
-        ans[data[i][1]]=sum/count;
+        for(int j=1;j<data.size();j++){
+            if(data[i][1]==temp){
+                sum+=stof(data[i][2]);
 
+                if(stof(data[i][2])>0){
+                    count++;
+                }
+            }
+        }
+        ratings[temp]=sum/count;
     }
     
 
@@ -59,26 +107,27 @@ int main() {
 
     string line;
     vector<vector<string>> data;
-    map <string ,float> ans;
+    vector<float> avg_rating;
+    map <string ,float> ratings;
+    vector<string> users; 
+    vector<string> movies;
+    
+    //Read data from csv file
+    read_file(data, line, users, movies);
 
-    read_file(data, line);
+       
+    //Calculate similarities between users
+    similarities(data, avg_rating);
 
-    recommend(data, ans); 
-
-
-   /*for(int i=0;i<data.size();i++){
-    for(int j=0;j<data[i].size();j++){
-        cout<<data[i][j]<<" ";
-    }
-    cout<<endl;
-   }*/
+    //Recoomend Movies
+    recommend(data, ratings); 
 
       
    cout<<"Recommended Movies:"<<endl; 
    vector<string> keys;
    //Copy movie names based on recommendation to vector ans
    
-   for (const auto& pair : ans) {
+   for (const auto& pair : ratings) {
         keys.push_back(pair.first);
     }
 
